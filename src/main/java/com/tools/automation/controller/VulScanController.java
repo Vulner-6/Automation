0b521898892d790cf3bd2d339b7fa3edf2fdfd81
Vulner-6 <1141613511@qq.com>
2020-01-20 21:12:42 +0800

    2020.1.20

--- src/main/java/com/tools/automation/controller/VulScanController.java
+++ src/main/java/com/tools/automation/controller/VulScanController.java
@@ -2,6 +2,9 @@ package com.tools.automation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class VulScanController
@@ -11,4 +14,14 @@ public class VulScanController
     {
         return "universalScan";
     }
+
+    @PostMapping("/universalScan")
+    public String universalScan(HttpServletRequest request)
+    {
+        String target=request.getParameter("target");
+        System.out.println(target);
+        return "universalScan";
+    }
+
+
 }
