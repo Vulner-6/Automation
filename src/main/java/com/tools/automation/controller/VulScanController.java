109b5889b46ffc68c661e2fe9233e61f1cc2bcc2
Vulner-6 <1141613511@qq.com>
2020-01-24 22:55:48 +0800

    2020.1.24

--- src/main/java/com/tools/automation/controller/VulScanController.java
+++ src/main/java/com/tools/automation/controller/VulScanController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class VulScanController
 {
@@ -43,22 +46,22 @@ public class VulScanController
             System.out.println("上传的文件内容为空");
             return "universalScan";
         }
+        //读取文件内容，进行批量验证
         else
         {
             //获取文件名
             String fileName=targetsInputFile.getOriginalFilename();
             //获取文件的后缀名
             String suffixName=fileName.substring(fileName.lastIndexOf("."));
-            //设置文件存储路径
-            String filePath=Class.class.getClass().getResource("/").getPath();
+            //设置文件存储路径，这里写死了，后期可以配置虚拟路径
+            String filePath="E:\\Programming\\Projects\\automation\\uploadFiles\\";
             String path=filePath+fileName;
-            System.out.println(filePath);
-            /*
+
             File dest=new File(path);
             //检测是否存在目录
             if(!dest.getParentFile().exists())
             {
-                dest.getParentFile().mkdir();  //新建文件夹
+                dest.getParentFile().mkdir();  //getParentFile的作用是新建/xx/a.txt，否则则是/xx/a.txt/，会无法创建
             }
 
             try
@@ -71,7 +74,7 @@ public class VulScanController
                 e.printStackTrace();
             }
 
-             */
+
 
         }
 
