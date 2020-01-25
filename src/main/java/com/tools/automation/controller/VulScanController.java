454d6dd1d4618b32da9256399fc7b1682e6c41cb
Vulner-6 <1141613511@qq.com>
2020-01-25 20:20:07 +0800

    2020.1.25

--- src/main/java/com/tools/automation/controller/VulScanController.java
+++ src/main/java/com/tools/automation/controller/VulScanController.java
@@ -1,5 +1,6 @@
 package com.tools.automation.controller;
 
+import com.tools.automation.support.FileOperation;
 import com.tools.automation.support.HttpsUtils;
 import com.tools.automation.support.UniversalScan;
 import okhttp3.OkHttpClient;
@@ -8,9 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
 
 @Controller
 public class VulScanController
@@ -49,33 +47,7 @@ public class VulScanController
         //读取文件内容，进行批量验证
         else
         {
-            //获取文件名
-            String fileName=targetsInputFile.getOriginalFilename();
-            //获取文件的后缀名
-            String suffixName=fileName.substring(fileName.lastIndexOf("."));
-            //设置文件存储路径，这里写死了，后期可以配置虚拟路径
-            String filePath="E:\\Programming\\Projects\\automation\\uploadFiles\\";
-            String path=filePath+fileName;
-
-            File dest=new File(path);
-            //检测是否存在目录
-            if(!dest.getParentFile().exists())
-            {
-                dest.getParentFile().mkdir();  //getParentFile的作用是新建/xx/a.txt，否则则是/xx/a.txt/，会无法创建
-            }
-
-            try
-            {
-                targetsInputFile.transferTo(dest);
-                System.out.println("写入成功");
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-
-
-
+            FileOperation.storeFile(targetsInputFile,"E:\\Programming\\Projects\\automation\\uploadFiles\\");
         }
 
 
