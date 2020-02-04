9cdae1124c271406ca760e75cbcadfd599ac67a9
Vulner-6 <1141613511@qq.com>
2020-02-04 20:44:11 +0800

    2020.02.04

--- src/main/java/com/tools/automation/controller/VulScanController.java
+++ src/main/java/com/tools/automation/controller/VulScanController.java
@@ -16,6 +16,8 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 
 @Controller
@@ -61,24 +63,99 @@ public class VulScanController
     @PostMapping("/singleScan")
     public String singleScan(
             @RequestParam(value = "target",required = false) String target,
-            @RequestParam(value = "poc",required = false) String[] pocs)
+            @RequestParam(value = "poc",required = false) String[] pocs,
+            @RequestParam(value = "allPoc",required = false) String allPoc
+            )
     {
-        //根据提交的插件，初始化单个目标扫描插件列表,初始化为全都不存在漏洞
-        for(String pocName:pocs)
+        //假如不是加载全部插件，而是加载用户自己勾选的插件
+        if(pocs!=null&&pocs.length>0&&allPoc==null)
         {
-            this.singleScanResult.put(pocName,false);
-        }
-        //声明线程计数器数量，方便后面的异步传输
-        final CountDownLatch latch=new CountDownLatch(pocs.length);
-        try
-        {
-            Object obj=this.cls.newInstance();
+            //根据提交的插件，初始化单个目标扫描插件列表,初始化为全都不存在漏洞
             for(String pocName:pocs)
             {
-                //如果获取的方法需要提供参数，那么就必须要提供参数类型。比如这里
-                Method method=this.cls.getMethod(pocName,OkHttpClient.class,String.class,
-                        HashMap.class,Method.class,CountDownLatch.class);
-                method.invoke(obj,this.okHttpClient,target,this.singleScanResult,method,latch);  //运行该方法的时候，也需要传递参数值
+                this.singleScanResult.put(pocName,false);
+            }
+            //声明线程计数器数量，方便后面的异步传输
+            final CountDownLatch latch=new CountDownLatch(pocs.length);
+            try
+            {
+                Object obj=this.cls.newInstance();
+                for(String pocName:pocs)
+                {
+                    //如果获取的方法需要提供参数，那么就必须要提供参数类型。比如这里
+                    Method method=this.cls.getMethod(pocName,OkHttpClient.class,String.class,
+                            HashMap.class,Method.class,CountDownLatch.class);
+                    method.invoke(obj,this.okHttpClient,target,this.singleScanResult,method,latch);  //运行该方法的时候，也需要传递参数值
+                }
+                //测试打印结果
+                try
+                {
+                    latch.await();  //等待异步验证全部结束，再执行这里的主线程
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                System.out.println(singleScanResult);
+            }
+            catch (InstantiationException e)
+            {
+                e.printStackTrace();
+            }
+            catch (IllegalAccessException e2)
+            {
+                e2.printStackTrace();
+            }
+            catch (NoSuchMethodException e3)
+            {
+                e3.printStackTrace();
+            }
+            catch (InvocationTargetException e4)
+            {
+                e4.printStackTrace();
+            }
+        }
+        //假如加载全部poc
+        if(allPoc!=null&&allPoc.equals("selectAll"))
+        {
+            Method[] methods=this.cls.getDeclaredMethods();
+            //利用set进行去重
+            Set<String> set=new HashSet<String>();
+            for(Method m:methods)
+            {
+                set.add(m.getName());
+            }
+            String[] uniqueResult=(String[])set.toArray(new String[set.size()]);
+            //声明线程计数器数量，方便后面的异步传输
+            final CountDownLatch latch=new CountDownLatch(uniqueResult.length);
+            //重新根据方法名，加载方法，使用方法。
+            for(String methodName:uniqueResult)
+            {
+                this.singleScanResult.put(methodName,false); //得提前初始化一下扫描结果值
+                try
+                {
+                    Object obj=this.cls.newInstance();
+                    Method method=this.cls.getMethod(methodName,OkHttpClient.class,String.class,HashMap.class,
+                            Method.class,CountDownLatch.class);
+                    method.invoke(obj,this.okHttpClient,target,this.singleScanResult,method,latch);
+                }
+                catch (NoSuchMethodException e)
+                {
+                    e.printStackTrace();
+                }
+                catch (IllegalAccessException e2)
+                {
+                    e2.printStackTrace();
+                }
+                catch (InstantiationException e3)
+                {
+                    e3.printStackTrace();
+                }
+                catch (InvocationTargetException e4)
+                {
+                    e4.printStackTrace();
+                }
+
             }
             //测试打印结果
             try
@@ -91,22 +168,7 @@ public class VulScanController
             }
             System.out.println(singleScanResult);
         }
-        catch (InstantiationException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IllegalAccessException e2)
-        {
-            e2.printStackTrace();
-        }
-        catch (NoSuchMethodException e3)
-        {
-            e3.printStackTrace();
-        }
-        catch (InvocationTargetException e4)
-        {
-            e4.printStackTrace();
-        }
+
 
         return "singleScan";
     }
